4ece491bd5ab82f7e76e6098d14605d522ae4179
alfredpm <alfredpermez@gmail.com>
2020-11-20 14:45:34 +0100

    Ajout de la copie du projet de cours à la branche test1.

---
+++ WebContent/WEB-INF/DAO.jsp
@@ -0,0 +1,40 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!-- Apparemment il faudrait utiliser l'uri : ".../core_rt" mais il ne fonctionne pas ici -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>DAOperso - JSP</title>
+</head>
+<body>
+<h1>DAOperso - JSP</h1>
+<p>
+	<c:out value="La bibliothèque JSTL est fonctionnelle."/>
+</p>
+
+<h2>Liste de jeux :</h2>
+<ul>
+	<c:forEach items="${listeJeux}" var="jeu">
+		<li>
+		<c:out value="${jeu.titre}"></c:out>
+		</li>
+	</c:forEach>
+</ul>	
+
+<h2>Ajout de jeu</h2>
+<p>Attention, l'apostrophe, les guillemets et les parenthèses ne sont pas supportées par notre système. Veuillez vous abstenir d'entrer un titre ou un genre en contenant.<br>Ne vous inquiétez pas, ce problème sera bientôt résolu. Nous travaillons dur pour vous permettre d'ajouter vos jeux favoris à nos services.</p>
+<form  method="post" action="dao">
+	<label for="id"> Id : </label>
+	<input type="number" id="addId" name="addId"/>
+	<label for="titre"> Titre : </label>
+	<input type="text" id="addTitre" name="addTitre"/>
+	<label for="genre"> Genre : </label>
+	<input type="text" id="addGenre" name="addGenre"/>
+	<input type="submit"/>  
+</form>
+
+</body>
+</html>
---
+++ src/fr/polytech/Jeux.java
@@ -0,0 +1,134 @@
+package fr.polytech;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+
+//Couche métier de l'application, reliée à la base de donnée DB_DAOperso
+//et plus particulièrement au tableau éponyme.
+public class Jeux {
+	// Son seul rôle est donc de récupérer la liste des jeux.
+	/*Dans la classe étudiants :
+	 * - On charge le driver(on utilise Class du java util, pas lang)
+	 * 		try {
+	 * 			Class.forName("com.mysql.cj.jdbc.Driver");
+	 * 		} catch (ClassNotFoundException e) {syso("Le driver n'est pas chargé.");}
+	 *
+	 * - On doit définir 3 objets :
+ 	 * 		- Connexion : représente la connexion à la base de donnée
+	 * 		- Statement : permet d'écrire les requêtes sql
+	 * 		- ResultSet : sauvegarde les résultats pour les transmettre à la jsp
+	 */
+	
+	private static String driverClass = "com.mysql.cj.jdbc.Driver";
+	private static String url = "jdbc:mysql://localhost:3306/db_sd?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
+	// Pourquoi ?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC ? ça sort de nulle part, mais ça règle mon soucis de SQLException
+	private static String user = "root";
+	private static String secret = "root";
+	
+	private static Connection connection;
+
+	public static void seConnecter() {
+		//Factorisation de la connexion
+		//Pas idéal, on est quand même obligé d'appeler un bout de code à tous les débuts de méthode.
+		try {
+			Class.forName(driverClass);	//Donne une erreur avant la def de forName, c'est normal
+		} catch (ClassNotFoundException e) {
+			System.out.println("Le driver MySQL n'est pas chargé.");
+		}
+
+		try {
+		// Connexion
+			connection = DriverManager.getConnection(url, user, secret);
+		} catch	(SQLException e) {
+			System.out.println("Problème de connexion à db_daoperso. "+e);
+		}
+	}
+	
+	
+	//Modifier un jeu
+	//Supprimer un jeu
+	//Renvoyer une liste de jeux à partir d'une recherche textuelle (paginer les résultats avec bootstrap?)
+	//Envoyer le tout sur git dans une branche "test1"
+
+	public List<Jeu> listerTousLesJeux(){
+		List<Jeu> resultat = new ArrayList<Jeu>();
+
+		seConnecter();
+		
+		// Initialisation
+		Statement statement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			statement = connection.createStatement();
+		// Exécuter une requête
+			resultSet = statement.executeQuery("SELECT * FROM `jeux`");
+		// Récupération des données
+			while(resultSet.next()) {
+				int id = resultSet.getInt("idJ");
+				String titre = resultSet.getString("titre");
+				String genre = resultSet.getString("genre");
+				resultat.add(new Jeu(id, titre, genre));
+			}
+		} catch (SQLException e) {
+			System.out.println("Problème de connexion à db_daoperso. "+e);
+		} finally {
+		//Clore la connexion
+			try{
+				if (connection!=null) {connection.close();}
+				if (statement!=null) {statement.close();}
+				if (resultSet!=null) {resultSet.close();}
+			} catch (Exception e) {System.out.println("Problème de fin de connexion.");}
+		}
+		//Envoie des données extraites
+		return resultat;
+	}
+
+	public void ajouterJeu(Jeu jeu) {
+		ajouterJeu(jeu.getId(),jeu.getTitre(),jeu.getGenre());
+	}
+	public void ajouterJeu(HttpServletRequest request) {
+		ajouterJeu(Integer.parseInt(request.getParameter("addId")), request.getParameter("addTitre"),request.getParameter("addGenre"));	
+	}
+	public void ajouterJeu(int id, String titre, String genre) {
+		String requeteSQL="INSERT INTO `jeux`(`idJ`, `titre`, `genre`) VALUES ("+id+",'"+titre+"','"+genre+"')";
+
+		seConnecter();
+		
+		// Initialisation
+		PreparedStatement prepstatement = null;
+		ResultSet resultSet = null;
+		
+		try {
+		//Prévention des failles d'injection SQL via prepareStatement
+			prepstatement = connection.prepareStatement("INSERT INTO `jeux`(`idJ`, `titre`, `genre`) VALUES (?,?,?);");
+		// Construire la requête
+			prepstatement.setInt(1, id);
+			prepstatement.setString(2, titre);
+			prepstatement.setString(3, genre);
+		// Exécution de la requête
+			prepstatement.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("Problème de connexion à db_daoperso. "+e+requeteSQL);
+		} finally {
+			try{
+				if (connection!=null) {connection.close();}
+				if (prepstatement!=null) {prepstatement.close();}
+				if (resultSet!=null) {resultSet.close();}
+			} catch (Exception e) {System.out.println("Problème de fin de connexion.");}
+		}
+	}
+
+
+
+}
