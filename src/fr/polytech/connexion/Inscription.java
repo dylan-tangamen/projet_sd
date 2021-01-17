76664fb32eccbd067ac500778fe0e3e06f0282ee
Dylan Tangamen <tangamendylan@mail.com>
2021-01-17 20:21:33 +0100

    Modif jsp inscription

--- src/fr/polytech/connexion/Inscription.java
+++ src/fr/polytech/connexion/Inscription.java
@@ -11,10 +11,34 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Inscription")
 public class Inscription extends HttpServlet {
     public static final String VUE = "/WEB-INF/Inscription.jsp";
+    public static final String CHAMP_EMAIL = "email";
+    public static final String CHAMP_PASS = "motdepasse";
+    public static final String CHAMP_CONF = "confirmation";
+    public static final String CHAMP_NOM = "nom";
 	
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
         this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
+    
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+        /* Récupération des champs du formulaire. */
+        String email = request.getParameter( CHAMP_EMAIL );
+        String motDePasse = request.getParameter( CHAMP_PASS );
+        String confirmation = request.getParameter( CHAMP_CONF );
+        String nom = request.getParameter( CHAMP_NOM );
+
+        try {
+            validationEmail( email );
+            validationMotsDePasse( motDePasse, confirmation );
+            validationNom( nom );
+        } catch (Exception e) {
+            /* Gérer les erreurs de validation ici. */
+        }
+    }
+
+    private void validationEmail( String email ) throws Exception{}
+    private void validationMotsDePasse( String motDePasse, String confirmation ) throws Exception{}
+    private void validationNom( String nom ) throws Exception{}
 }
        
     
